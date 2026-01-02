8ab399f58a80a124d9d6f5e8688b6dc09fd11f21
Biniyamgirma <119936137+Biniyamgirma@users.noreply.github.com>
2026-01-03 00:03:38 +0300

    adjusted footers page know i will try to make it mobile friendly

--- ethchess-landing-page/app/Footer.tsx
+++ ethchess-landing-page/app/Footer.tsx
@@ -1,97 +1,111 @@
 import React from "react";
+import Image from "next/image";
+import ETHchessLogo from "../public/image/ethchess.png";
+import Email from "../public/image/email.png";
+import Phone from "../public/image/phone.png";
+
+import Youtube from "../public/image/youtube.png";
+import Instagram from "../public/image/instagram.png";
+import Facebook from "../public/image/facebook.png";
+import Tiwtter from "../public/image/tiwtter.png";
+import Person from "../public/image/profile.png";
+import Search from "../public/image/serch.png";
+import Gmail from "../public/image/gmail.png";
 
 function Footer() {
   return (
     <footer className="text-white bg-[#141731] ">
       <div className="flex justify-between px-12 py-8">
         <div className="space-y-8 w-1/3">
-          <div className="flex h-12 w-fit items-center  justify-center space-x-2 text-2xl">
+          <div className="flex h-12 w-fit items-center font-bold justify-center space-x-2 text-2xl">
             ETH
-            <img
-              className="size-8"
-              src="https://plus.unsplash.com/premium_vector-1689096818551-edb79a6fa3da?q=80&w=580&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt=""
-            />{" "}
-            CHESS
+            <Image src={ETHchessLogo} alt="" className="size-8" /> CHESS
           </div>
           <div className="  ">
-            <p className=" text-lg">
+            <p className=" text-lg font-light text-[#D6DCE8]">
               Connecting chess players, tournaments, and community across
               Ethiopia, one move at a time.
             </p>
           </div>
-          <div className="flex-col space-y-4">
-            <div className="flex size-6 space-x-2">
-              <img
-                src="https://plus.unsplash.com/premium_vector-1689096818551-edb79a6fa3da?q=80&w=580&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                alt=""
-              />
-              <p>Support@ethchess.com</p>
+          <div className="flex-col space-y-4 font-light text-[#D6DCE8]">
+            <div className="">
+              <a href="" className="flex size-6 space-x-2">
+                <Image src={Email} alt="email icon" className="size-6" />
+                <p>Support@ethchess.com</p>
+              </a>
             </div>
             <div className="flex size-6 space-x-2">
-              <img
-                src="https://plus.unsplash.com/premium_vector-1689096818551-edb79a6fa3da?q=80&w=580&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                alt=""
-              />
-              <p>+252972788430</p>
+              <a href="" className="flex size-6 space-x-2">
+                <Image src={Phone} alt="phone" className="size-6" />
+                <p>+252972788430</p>
+              </a>
             </div>
             <div className="flex size-6 space-x-2">
-              <img
-                src="https://plus.unsplash.com/premium_vector-1689096818551-edb79a6fa3da?q=80&w=580&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                alt=""
-              />
-              <p>Ethiopia</p>
+              <a href="" className="flex size-6 space-x-2">
+                <Image src={Email} alt="location" className="size-6" />
+                <p>Ethiopia</p>
+              </a>
             </div>
           </div>
-          <div className="flex size-8 space-x-6 ">
-            <img
-              src="https://plus.unsplash.com/premium_vector-1683141234968-b4f861c0546a?q=80&w=666&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt=""
-            />
-            <img
-              src="https://plus.unsplash.com/premium_vector-1683141234968-b4f861c0546a?q=80&w=666&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt=""
-            />
-            <img
-              src="https://plus.unsplash.com/premium_vector-1683141234968-b4f861c0546a?q=80&w=666&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt=""
-            />
-            <img
-              src="https://plus.unsplash.com/premium_vector-1683141234968-b4f861c0546a?q=80&w=666&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt=""
-            />
-            <img
-              src="https://plus.unsplash.com/premium_vector-1683141234968-b4f861c0546a?q=80&w=666&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt=""
-            />
+          <div className="flex size-6 space-x-6 ">
+            <Image src={Instagram} alt="instagram logo image" />
+            <Image src={Tiwtter} alt="tiwtter log" />
+
+            <Image src={Facebook} alt="facebook logo" />
+            <Image src={Gmail} alt="gmail logo" />
+            <Image src={Youtube} alt="youtub logo" />
           </div>
         </div>
         <div className="flex pl-4 space-x-16">
-          <ul className="space-y-2 text-sm">
-            <li className="font-bold text-xl pb-2">Chess Community</li>
-            <li>Leader-board</li>
-            <li>Upcoming Tournaments</li>
-            <li>Blog / Brilliant Moves</li>
-            <li>Lichess Club Link</li>
+          <ul className="space-y-2 text-sm font-light text-[#D6DCE8]">
+            <li className="font-semibold text-xl pb-2">Chess Community</li>
+            <li>
+              <a href="">Leader-board</a>
+            </li>
+            <li>
+              <a href="">Upcoming Tournaments</a>
+            </li>
+            <li>
+              <a href="">Blog / Brilliant Moves</a>
+            </li>
+            <li>
+              <a href="">Lichess Club Link</a>
+            </li>
           </ul>
-          <ul className="space-y-2 text-sm">
-            <li className="font-bold text-xl pb-2">Chess Community</li>
-            <li>Leader-board</li>
-            <li>Upcoming Tournaments</li>
-            <li>Blog / Brilliant Moves</li>
-            <li>Lichess Club Link</li>
+          <ul className="space-y-2 text-sm font-light text-[#D6DCE8]">
+            <li className="font-semibold text-xl pb-2">Chess Community</li>
+            <li>
+              <a href="">Leader-board</a>
+            </li>
+            <li>
+              <a href="">Upcoming Tournaments</a>
+            </li>
+            <li>
+              <a href="">Blog / Brilliant Moves</a>
+            </li>
+            <li>
+              <a href="">Lichess Club Link</a>
+            </li>
           </ul>
-          <ul className="space-y-2 text-sm">
-            <li className="font-bold text-xl pb-2 ">Chess Community</li>
-            <li>Leader-board</li>
-            <li>Upcoming Tournaments</li>
-            <li>Blog / Brilliant Moves</li>
-            <li>Lichess Club Link</li>
+          <ul className="space-y-2 text-sm font-light text-[#D6DCE8]">
+            <li className="font-semibold text-xl pb-2 ">Chess Community</li>
+            <li>
+              <a href="">Leader-board</a>
+            </li>
+            <li>
+              <a href="">Upcoming Tournaments</a>
+            </li>
+            <li>
+              <a href="">Blog / Brilliant Moves</a>
+            </li>
+            <li>
+              <a href="">Lichess Club Link</a>
+            </li>
           </ul>
         </div>
       </div>
-      <div className="h-8 mt-16 w-screen rounded-t-3xl bg-[#FFC610] text-center text-[#141731]">
-        copy 2025
+      <div className="h-8 mt-16 w-screen flex justify-center items-center rounded-t-3xl bg-[#FFC610] text-center text-[#141731]">
+        &copy; copy 2026 ETHchess ETHIOPIA
       </div>
     </footer>
   );
