49db2399399a9d14d56426208895eb1a1d7759d9
Biniyamgirma <119936137+Biniyamgirma@users.noreply.github.com>
2026-01-02 23:34:46 +0300

    adjusted Growing Chess Community page

--- ethchess-landing-page/app/Community.tsx
+++ ethchess-landing-page/app/Community.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import React from "react";
 import Peoples from "../public/image/peoples.png";
+import Wancha from "../public/image/wancha.png";
+import Clock from "../public/image/clock.png";
+
 function Community() {
   return (
     <section className="w-screen md:h-screen  bg-linear-45 px-6 from-[#392561] to-[#754CC7]">
@@ -15,53 +18,61 @@ function Community() {
       </div>
 
       <div className="flex flex-wrap justify-around items-center md:mt-24 space-y-4 md:space-y-0">
-        <div className=" space-y-4 border bg-[#2A364D4D] border-[#465573] rounded-2xl px-12 py-6">
+        <div className=" space-y-4 border bg-[#2A364D4D] border-[#465573] rounded-2xl size-64 py-6">
           <div className="p-4 mx-auto rounded-full size-20 bg-[#3C3167]">
             <Image src={Peoples} alt="" className="" />
           </div>
           <div className="flex justify-center items-end">
-            <h5 className="text-white text-2xl">1,247</h5>
-            <span className="text-[#2457C5] text-[14px] flex align-bottom">
+            <h5 className="text-white text-3xl font-bold">1,247</h5>
+            <span className="text-[#2457C5] font-bold text-[14px] flex align-bottom">
               +12%
             </span>
           </div>
-          <p className="text-center">Active players</p>
+          <p className="text-center font-semibold text-[#DAD8D8]">
+            Active players
+          </p>
         </div>
-        <div className=" space-y-4 border bg-[#2A364D4D] border-[#465573] rounded-2xl px-12 py-6">
+        <div className=" space-y-4 border bg-[#2A364D4D] border-[#465573] rounded-2xl size-64 py-6">
           <div className="p-4 mx-auto rounded-full size-20 bg-[#3C3167]">
-            <Image src={Peoples} alt="" className="" />
+            <Image src={Wancha} alt="" className="" />
           </div>
           <div className="flex justify-center items-end">
-            <h5 className="text-white text-2xl">1,247</h5>
-            <span className="text-[#2457C5] text-[14px] flex align-bottom">
-              +12%
+            <h5 className="text-white text-3xl font-bold">523</h5>
+            <span className="text-[#D8A353] font-bold text-[14px] flex align-bottom">
+              +8%
             </span>
           </div>
-          <p className="text-center">Active players</p>
+          <p className="text-center font-semibold text-[#DAD8D8] ">
+            Tournament Hosted
+          </p>
         </div>
-        <div className=" space-y-4 border bg-[#2A364D4D] border-[#465573] rounded-2xl px-12 py-6">
+        <div className=" space-y-4 border bg-[#2A364D4D] border-[#465573] rounded-2xl size-64 py-6">
           <div className="p-4 mx-auto rounded-full size-20 bg-[#3C3167]">
             <Image src={Peoples} alt="" className="" />
           </div>
           <div className="flex justify-center items-end">
-            <h5 className="text-white text-2xl">1,247</h5>
-            <span className="text-[#2457C5] text-[14px] flex align-bottom">
-              +12%
+            <h5 className="text-white text-3xl font-bold">12,847</h5>
+            <span className="text-[#00E1FF] font-bold text-[14px] flex align-bottom">
+              +24%
             </span>
           </div>
-          <p className="text-center">Active players</p>
+          <p className="text-center font-semibold text-[#DAD8D8]">
+            Game Played
+          </p>
         </div>
-        <div className=" space-y-4 border bg-[#2A364D4D] border-[#465573] rounded-2xl px-12 py-6">
+        <div className=" space-y-4 border bg-[#2A364D4D] border-[#465573] rounded-2xl size-64 py-6">
           <div className="p-4 mx-auto rounded-full size-20 bg-[#3C3167]">
-            <Image src={Peoples} alt="" className="" />
+            <Image src={Clock} alt="" className="" />
           </div>
           <div className="flex justify-center items-end">
-            <h5 className="text-white text-2xl">1,247</h5>
-            <span className="text-[#2457C5] text-[14px] flex align-bottom">
+            <h5 className="text-white text-3xl font-bold">1,247</h5>
+            <span className="text-[#8B2CF5] text-[14px] font-bold flex align-bottom">
               +12%
             </span>
           </div>
-          <p className="text-center">Active players</p>
+          <p className="text-center font-semibold text-[#DAD8D8]">
+            Active players
+          </p>
         </div>
       </div>
     </section>
