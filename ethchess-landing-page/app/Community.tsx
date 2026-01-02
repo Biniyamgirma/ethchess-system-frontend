8d1068eeaf8b325baa7547c4f5f348cf8cc7a74c
Biniyamgirma <119936137+Biniyamgirma@users.noreply.github.com>
2026-01-02 23:17:25 +0300

    adjusted the bug in UI service.tsx file

--- ethchess-landing-page/app/Community.tsx
+++ ethchess-landing-page/app/Community.tsx
@@ -4,11 +4,16 @@ import Peoples from "../public/image/peoples.png";
 function Community() {
   return (
     <section className="w-screen md:h-screen  bg-linear-45 px-6 from-[#392561] to-[#754CC7]">
-      <h3 className="text-center md:text-3xl py-6">Growing Chess Community</h3>
-      <p className="text-center md:text-xl mb-6">
-        Join thousands of chess enthusiasts who sre already improving their game
-        with ETHCHESS Club
-      </p>
+      <div className="py-6 space-y-4">
+        <h3 className="text-[28px] font-bold  md:text-4xl  text-white text-center">
+          Growing Chess Community
+        </h3>
+        <p className="text-center text-[12px] md:text-xl font-extralight">
+          Join thousands of chess enthusiasts who sre already improving their
+          game with ETHCHESS Club
+        </p>
+      </div>
+
       <div className="flex flex-wrap justify-around items-center md:mt-24 space-y-4 md:space-y-0">
         <div className=" space-y-4 border bg-[#2A364D4D] border-[#465573] rounded-2xl px-12 py-6">
           <div className="p-4 mx-auto rounded-full size-20 bg-[#3C3167]">
--- ethchess-landing-page/app/Services.tsx
+++ ethchess-landing-page/app/Services.tsx
@@ -1,97 +1,119 @@
 import Image from "next/image";
-import Crown from "../public/image/clock.png";
-
+import Wancha from "../public/image/wancha.png";
+import CrownPurple from "../public/image/crown.png";
+import Community from "../public/image/peoples.png";
+import Secure from "../public/image/secure.png";
+import Clock from "../public/image/clock.png";
+import Event from "../public/image/star.png";
+import Global from "../public/image/global.png";
+import Fast from "../public/image/fast.png";
 export default function Services() {
   return (
     <section className="w-full flex-col justify-center items-center  space-y-4 px-20 py-4 bg-linear-to-b from-[#1617361A] via-[#421B6C66] to-[#1617361A]">
-      <h2 className="text-[28px]  md:text-3xl text- text-white text-center">
-        Everything You Need To Excel
-      </h2>
-      <p className="text-center text-[12px] md:text-xl">
-        Our comprehensive platform provides all the tools and features you need
-        to improve your <br /> chess game and connect with the community{" "}
-      </p>
-      <div className=" flex flex-wrap justify-center w-full space-y-6 md:space-x-6">
-        <div className="flex-col px-6 py-8 justify-around w-64 space-y-4 bg-[#2A364D73] rounded-2xl">
+      <div className="py-6 space-y-4">
+        <h2 className="text-[28px] font-bold  md:text-4xl  text-white text-center">
+          Tournament System
+        </h2>
+        <p className="text-center text-[12px] md:text-xl font-extralight">
+          Our comprehensive platform provides all the tools and features you
+          need to improve your <br /> chess game and connect with the
+          community{" "}
+        </p>
+      </div>
+      <div className=" flex flex-wrap justify-center w-full gap-4 md:gap-6 pb-8">
+        <div className="flex-col px-6 py-8 justify-around w-64 space-y-4 bg-[#2A364D73] border border-[#465573] rounded-2xl">
           <div className="p-2 size-10 bg-[#4F6184] rounded-2xl mx-auto">
-            <Image src={Crown} alt="" className="" />
+            <Image src={Wancha} alt="" className="" />
           </div>
-          <h5 className="text-sm text-center md:text-xl">
+          <h5 className="text-sm text-center font-bold md:text-lg">
+            Tournament System
+          </h5>
+          <p className="text-[10px] text-center font-normal text-[#E3E1E1]">
+            Participate in a weekly tournaments with different time controls and
+            formats.win the price and climb the rank
+          </p>
+        </div>
+        <div className="flex-col px-6 py-8 justify-around w-64 space-y-4 bg-[#2A364D73] border border-[#465573] rounded-2xl">
+          <div className="p-2 size-10 bg-[#4F6184] rounded-2xl mx-auto">
+            <Image src={CrownPurple} alt="" className="" />
+          </div>
+          <h5 className="text-sm text-center font-bold md:text-lg">
             Dynamic Leaderboard
           </h5>
-          <p className="text-[10px] text-center md:text-sm">
-            Track your progress with real-time ranking across different
-            categories and time period
-          </p>
-        </div>
-        <div className="flex-col px-6 py-8 justify-around w-64 space-y-4 bg-[#2A364D73] rounded-2xl">
-          <div className="p-2 size-10 bg-[#4F6184] rounded-2xl mx-auto">
-            <Image src={Crown} alt="" className="" />
-          </div>
-          <h5 className="text-sm text-center">Dynamic Leaderboard</h5>
           <p className="text-[10px] text-center">
             Track your progress with real-time ranking across different
             categories and time period
           </p>
         </div>
-        <div className="flex-col px-6 py-8 justify-around w-64 space-y-4 bg-[#2A364D73] rounded-2xl">
+        <div className="flex-col px-6 py-8 justify-around w-64 space-y-4 bg-[#2A364D73] border border-[#465573] rounded-2xl">
           <div className="p-2 size-10 bg-[#4F6184] rounded-2xl mx-auto">
-            <Image src={Crown} alt="" className="" />
+            <Image src={Community} alt="" className="" />
           </div>
-          <h5 className="text-sm text-center">Dynamic Leaderboard</h5>
+          <h5 className="text-sm text-center font-bold md:text-lg">
+            Active community
+          </h5>
           <p className="text-[10px] text-center">
-            Track your progress with real-time ranking across different
-            categories and time period
+            Connect with players worldwide,join discussion, and share your chess
+            knowledge
           </p>
         </div>
-        <div className="flex-col px-6 py-8 justify-around w-64 space-y-4 bg-[#2A364D73] rounded-2xl">
+        <div className="flex-col px-6 py-8 justify-around w-64 space-y-4 bg-[#2A364D73] border border-[#465573] rounded-2xl">
           <div className="p-2 size-10 bg-[#4F6184] rounded-2xl mx-auto">
-            <Image src={Crown} alt="" className="" />
+            <Image src={Secure} alt="" className="" />
           </div>
-          <h5 className="text-sm text-center">Dynamic Leaderboard</h5>
+          <h5 className="text-sm text-center font-bold md:text-lg">
+            Secure Platform
+          </h5>
           <p className="text-[10px] text-center">
-            Track your progress with real-time ranking across different
-            categories and time period
+            Enterprise-grade security with encrypted data and fair play
+            monitoring
           </p>
         </div>
-        <div className="flex-col px-6 py-8 justify-around w-64 space-y-4 bg-[#2A364D73] rounded-2xl">
+        <div className="flex-col px-6 py-8 justify-around w-64 space-y-4 bg-[#2A364D73] border border-[#465573] rounded-2xl">
           <div className="p-2 size-10 bg-[#4F6184] rounded-2xl mx-auto">
-            <Image src={Crown} alt="" className="" />
+            <Image src={Clock} alt="" className="" />
           </div>
-          <h5 className="text-sm text-center">Dynamic Leaderboard</h5>
+          <h5 className="text-sm text-center font-bold md:text-lg">
+            Event scheduling
+          </h5>
           <p className="text-[10px] text-center">
-            Track your progress with real-time ranking across different
-            categories and time period
+            Never miss a tournament club event with our smart notification
+            system
           </p>
         </div>
-        <div className="flex-col px-6 py-8 justify-around w-64 space-y-4 bg-[#2A364D73] rounded-2xl">
+        <div className="flex-col px-6 py-8 justify-around w-64 space-y-4 bg-[#2A364D73] border border-[#465573] rounded-2xl">
           <div className="p-2 size-10 bg-[#4F6184] rounded-2xl mx-auto">
-            <Image src={Crown} alt="" className="" />
+            <Image src={Event} alt="" className="" />
           </div>
-          <h5 className="text-sm text-center">Dynamic Leaderboard</h5>
+          <h5 className="text-sm text-center font-bold md:text-lg">
+            Premium Profile
+          </h5>
           <p className="text-[10px] text-center">
-            Track your progress with real-time ranking across different
-            categories and time period
+            Show case your achievements with customizable premium profile and
+            statics
           </p>
         </div>
-        <div className="flex-col px-6 py-8 justify-around w-64 space-y-4 bg-[#2A364D73] rounded-2xl">
+        <div className="flex-col px-6 py-8 justify-around w-64 space-y-4 bg-[#2A364D73] border border-[#465573] rounded-2xl">
           <div className="p-2 size-10 bg-[#4F6184] rounded-2xl mx-auto">
-            <Image src={Crown} alt="" className="" />
+            <Image src={Fast} alt="" className="" />
           </div>
-          <h5 className="text-sm text-center">Dynamic Leaderboard</h5>
+          <h5 className="text-sm text-center font-bold md:text-lg">
+            Lichess integration
+          </h5>
           <p className="text-[10px] text-center">
-            Track your progress with real-time ranking across different
-            categories and time period
+            Seamlessly sync with lichess for game import and advanced analytics
           </p>
         </div>
-        <div className="flex-col px-6 py-8 md:pb-0 justify-around w-64 space-y-4 bg-[#2A364D73] rounded-2xl">
+        <div className="flex-col px-6 py-8 justify-around w-64 space-y-4 bg-[#2A364D73] border border-[#465573] rounded-2xl">
           <div className="p-2 size-10 bg-[#4F6184] rounded-2xl mx-auto">
-            <Image src={Crown} alt="" className="" />
+            <Image src={Global} alt="" className="" />
           </div>
-          <h5 className="text-sm text-center">Dynamic112 Leaderboard</h5>
+          <h5 className="text-sm text-center font-bold md:text-lg">
+            Global Reach
+          </h5>
           <p className="text-[10px] text-center">
-            Track your progress with real-time ranking across different
-            categories and time period
+            Join players from around the world in our international chess
+            community
           </p>
         </div>
       </div>
